6100eb44cd5be8879e1da4529f92a5dc009643c4
Eddie Krystowski <eddiekrystowski@gmail.com>
2021-09-02 17:23:41 -0400

    add super basic ui buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import MenuBar from './MenuBar';
+
+import './index.css'; 
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mode: 'create'
+        }
+    }
+
+    handleStateSwitch() {
+        console.log('Switching state...');
+        console.log(this.state);
+        this.setState({
+            mode: this.state.mode === 'create' ? 'proof' : 'create'
+        });
+    }
+
     render() {
         return (
             <div className="app">
+                <MenuBar mode={this.state.mode} onStateSwitch={() => { this.handleStateSwitch(); }}></MenuBar>
                 <div id="paper-container"></div>
             </div>
         );
