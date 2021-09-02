3cd5d62e044029fcbcd7cde73a91721bb25c8a77
Eddie Krystowski <eddiekrystowski@gmail.com>
2021-09-02 13:22:27 -0400

    add app class

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,10 @@
-//cool app
+import React from 'react';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import * as joint from 'jointjs';
+
+import App from './App.jsx';
 
 console.log("Starting...");
 console.log(joint);
+
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
