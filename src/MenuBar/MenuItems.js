71c9bc89596fb633b3ae936782f07398ae187401
Jack Rutland <jackrrutland@gmail.com>
2021-09-04 19:48:33 -0400

    export

--- src/MenuBar/MenuItems.js
+++ src/MenuBar/MenuItems.js
@@ -2,6 +2,8 @@ import importIcon from './MenuIcons/import.png';
 import exportIcon from './MenuIcons/export.png';
 import logoIcon from './MenuIcons/logo.png';
 
+import { graph } from '../index.js';
+
 const MenuItems = [
   {
     text: '',
@@ -37,6 +39,18 @@ const MenuItems = [
   //Funtions for the above MenuItems
 function exportEG() {
   console.log('Exporting...');
+  let graphJSON = graph.toJSON();
+  const file = new Blob([JSON.stringify(graphJSON)], { type: 'application/json'});
+    const a = document.createElement("a");
+    let url = URL.createObjectURL(file);
+    a.href = url;
+    a.download = 'graph.json';
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(function () {
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+    }, 0);
 }
 
 function importEG() {
