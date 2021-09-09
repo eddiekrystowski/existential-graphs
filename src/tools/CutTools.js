0b0d2036c004b2b7833cc747cede7e41a90d4044
Eddie Krystowski <eddiekrystowski@gmail.com>
2021-09-09 16:59:35 -0400

    initial resize algorithm

--- src/tools/CutTools.js
+++ src/tools/CutTools.js
@@ -1,5 +1,6 @@
 import * as joint from "jointjs"
-import { paper } from "../index.js"
+import $ from 'jquery';
+import { graph, paper } from "../index.js"
 import { createResizeTool } from "./ResizeTool.js" 
 
 
@@ -14,23 +15,30 @@ export function addCutTools(element) {
     let NWresizeTool = new joint.elementTools.Button(createResizeTool({
         x: "0%",
         y:"0%",
-        cursor: "nw_resize"
+        cursor: "nw-resize"
     }));
     let NEresizeTool = new joint.elementTools.Button(createResizeTool({
         x: "100%",
         y:"0%",
-        cursor: "ne_resize"
+        cursor: "ne-resize"
     }));
     let SWresizeTool = new joint.elementTools.Button(createResizeTool({
         x: "0%",
         y:"100%",
-        cursor: "sw_resize"
+        cursor: "sw-resize"
     }));
     let SEresizeTool = new joint.elementTools.Button(createResizeTool({
         x: "100%",
         y:"100%",
-        cursor: "se_resize"
+        cursor: "se-resize"
     }));
+
+    console.log('nw resize', NWresizeTool);
+
+    $(NWresizeTool.el).on('mousedown', resize_mousedown);
+    $(NEresizeTool.el).on('mousedown', resize_mousedown);
+    $(SWresizeTool.el).on('mousedown', resize_mousedown);
+    $(SEresizeTool.el).on('mousedown', resize_mousedown);
 
     let rect_tools = [boundaryTool, NWresizeTool, NEresizeTool, SEresizeTool, SWresizeTool];
 
@@ -45,4 +53,60 @@ export function addCutTools(element) {
     //     element.toFront();
     // });
     // --- end of paper events -----
+}
+
+
+//$('.nw-resize').on('mousedown', resize_mousedown);
+
+let mousedown_pos = {
+    x: 0,
+    y: 0
+}
+
+let current_pos = {
+    x: 0,
+    y: 0
+}
+
+function resize_mousedown(event) {
+    const target = graph.getCell($(event.target).parent().attr('model-id'));
+    console.log('target model', target);
+    mousedown_pos = {
+        x: event.clientX,
+        y: event.clientY
+    }
+    
+    $(document).on('mouseup', resize_mouseup);
+    $(document).on('mousemove', { target, self: event.target },  resize_mousemove);
+    event.stopPropagation();
+}
+
+function resize_mousemove(event) {
+    current_pos = {
+        x: event.clientX,
+        y: event.clientY
+    }
+
+    const size = {
+        width: event.data.target.attributes.attrs.rect.width,
+        height: event.data.target.attributes.attrs.rect.height
+    };
+
+    console.log(event.data.target);
+    console.log(size);
+    const delta = {
+        x: current_pos.x - mousedown_pos.x,
+        y: current_pos.y - mousedown_pos.y
+    }
+
+    event.data.target.attr('rect/width', Math.max(80, size.width + delta.x));
+    event.data.target.attr('rect/height', Math.max(80, size.height + delta.y));
+    
+    mousedown_pos = Object.assign({}, current_pos);
+
+}
+
+function resize_mouseup (event) {
+    $(document).off('mouseup', resize_mouseup);
+    $(document).off('mousemove', resize_mousemove);
 }
