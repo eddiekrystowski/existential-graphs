a91f05b9085bc2cc11be5b263d01e9c502beae4d
Eddie Krystowski <eddiekrystowski@gmail.com>
2021-09-23 15:41:25 -0400

    add all buttons

--- src/util/proof-util.js
+++ src/util/proof-util.js
@@ -1,5 +1,24 @@
 
+export const inferenceInsertion = function(model) {
+    console.log('INSERTING via INFERENCE');
+}
+
+export const inferenceErasure = function(model) {
+    console.log('ERASING via INFERENCE');
+}
+
 export const insertDoubleCut = function(model) {
     console.log('INSERTING DOUBLE CUT');
 }
 
+export const deleteDoubleCut = function(model) {
+    console.log('DELETING DOUBLE CUT');
+}
+
+export const iteration = function(model) {
+    console.log('ITERATING');
+}
+
+export const deiterating = function(model) {
+    console.log('DEITERATING');
+}
