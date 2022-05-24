e21461d51263c0f137f13ff50a0b29dc2461e005
Eddie Krystowski <eddiekrystowski@gmail.com>
2022-05-23 23:25:19 -0400

    add comments and docstrings to History.jsx and HistoryItem.jsx

--- src/components/Paper/History/HistoryItem.jsx
+++ src/components/Paper/History/HistoryItem.jsx
@@ -4,14 +4,23 @@ import * as joint from 'jointjs';
 
 import Sheet from '../Sheet/Sheet';
 
+/**
+ * This class renders an individual JointJS paper inside a History component. 
+ * The actual data that makes up the paper is entirely derived from the History it is part of.
+ */
 export default class HistoryItem extends React.Component {
     constructor(props) {
         super(props);
 
+        // keeps track of joint paper object 
         this.jpaper = null;
+
+        // Sheet component for managing graph
         this.sheet = new Sheet(this);
     }
 
+    // if any of the properties of this component change, make sure cells are correct
+    // (only need to do this if it is the last item in the history since we could be making a new branch)
     componentDidUpdate(prevProps, prevState) {
         if(this.props.num === this.props.total -1 ){
             this.sheet.importCells(this.props.cells);
@@ -19,6 +28,7 @@ export default class HistoryItem extends React.Component {
         }
     }
 
+    // initialize component
     componentDidMount() {
         this.jpaper = new joint.dia.Paper({
             el: document.getElementById(this.props.id),
@@ -35,19 +45,19 @@ export default class HistoryItem extends React.Component {
 
         this.jpaper.setInteractivity(false);
         
+
+        //add and remove highlight on hover
         $(`#${this.props.id}`).on('mouseover', function() {
             $(this).closest('.history-item').addClass('history-selected');
             
         });
-
         $(`#${this.props.id}`).on('mouseout', function() {
             $(this).closest('.history-item').removeClass('history-selected');
         });
     }
 
-    refresh() {
-
-    }
+    //TODO: This was just sitting here empty, not sure if it is necessary to have or not
+    refresh() {}
 
     render() {
         const style = {
