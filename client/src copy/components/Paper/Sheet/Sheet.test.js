e5aa641030bf9d6005f42f7efb6db6deee04060c
Eddie Krystowski <eddiekrystowski@gmail.com>
2023-09-29 17:07:08 -0400

    Remove old client code from before stack migration
    Co-Authored-By: Joseph Krystowski <65693478+joekrystowski@users.noreply.github.com>

--- client/src copy/components/Paper/Sheet/Sheet.test.js
+++
@@ -1,2 +0,0 @@
-import { contains } from '../../../util/collisions';
-
--- client/src copy/util/otherUtil.js
+++
@@ -1,8 +0,0 @@
-export function cellInArray(cell, arr) {
-    for (const e of arr) {
-        if (cell.id === e.id) {
-            return true;
-        }
-    }
-    return false;
-} 
